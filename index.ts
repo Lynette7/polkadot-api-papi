df527b94f0c962851b0ffd6d94988b556c6bc5b1
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-11-08 12:12:20 +0300

    chore: RPC endpoint

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import process from "node:process";
 import { getWsProvider } from "polkadot-api/ws-provider/web";
 import { createClient, type PolkadotClient } from "polkadot-api";
 
@@ -7,3 +8,13 @@ function makeClient(endpoint: string): PolkadotClient {
     const client = createClient(provider);
     return client;
 }
+
+async function main() {
+    const polkadotClient = makeClient("wss://rpc.polkadot.io");
+    console.log({ polkadotClient });
+
+    console.log(`Done!`);
+    process.exit(0);
+}
+
+main();
