f35387126f9f81e87a866fc8992fd8fac86a7e69
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-11-12 17:35:24 +0300

    chore: account info, balance

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import process from "node:process";
 import { getWsProvider } from "polkadot-api/ws-provider/web";
-import { createClient, type PolkadotClient } from "polkadot-api";
+import { createClient, type PolkadotClient, type SS58String, } from "polkadot-api";
+import { dot } from "@polkadot-api/descriptors";
 
 function makeClient(endpoint: string): PolkadotClient {
     console.log(`Connecting to endpoint: ${endpoint}`);
@@ -10,14 +11,26 @@ function makeClient(endpoint: string): PolkadotClient {
 }
 
 async function printChainInfo(client: PolkadotClient) {
+    // This method should not be used in prod since chain info isn't standardized onchain yet
     const chainSpec = await client.getChainSpecData();
     const finalizedBlock = await client.getFinalizedBlock();
     console.log(`Connected to ${chainSpec.name} at block ${finalizedBlock.number}.`);
 }
 
+async function getBalance(polkadotClient: PolkadotClient, address: SS58String) {
+    const dotApi = polkadotClient.getTypedApi(dot);
+    const accountInfo = await dotApi.query.System.Account.getValue(address);
+    const { free, reserved } = accountInfo.data;
+    return free + reserved;
+}
+
 async function main() {
     const polkadotClient = makeClient("wss://rpc.polkadot.io");
     await printChainInfo(polkadotClient);
+
+    const address = "15DCZocYEM2ThYCAj22QE4QENRvUNVrDtoLBVbCm5x4EQncr";
+    const balance = await getBalance(polkadotClient, address);
+    console.log(`Address ${address} has a balnce of ${balance}.`);
 
     console.log(`Done!`);
     process.exit(0);
